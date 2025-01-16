4669314a4c3ab93eda3ae5afdd868205c4d612d6
Luiz Phelipe <luizphsantana@gmail.com>
2025-01-16 09:56:52 -0300

    Add project files.

---
+++ Consumindo-API-do-github/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Consumindo_API_do_github
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
