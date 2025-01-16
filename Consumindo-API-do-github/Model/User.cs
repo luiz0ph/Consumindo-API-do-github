dd833d6379c42063836b3e8165f713c50c4d91ca
Luiz Phelipe <luizphsantana@gmail.com>
2025-01-16 10:31:42 -0300

    Created the user model, main menu and user list

---
+++ Consumindo-API-do-github/Model/User.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Consumindo_API_do_github.Model
+{
+    public class User
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("login")]
+        public string? UserName { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("bio")]
+        public string? Bio { get; set; }
+
+        [JsonPropertyName("public_repos")]
+        public int PublicRepos { get; set; }
+
+        [JsonPropertyName("repos_url")]
+        public string ReposUrl { get; set; }
+    }
+}
--- Consumindo-API-do-github/Program.cs
+++ Consumindo-API-do-github/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Consumindo_API_do_github
+﻿using Consumindo_API_do_github.Model;
+
+namespace Consumindo_API_do_github
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // User list
+            List<User> users = new List<User>();
+            Menu();
+        }
+
+        public static void Menu()
+        {
+            #region Options menu
+            Console.Clear();
+            Console.WriteLine($"Escolha uma das opções abaixo: \n" +
+                $"1 - Adicionar usuario na lista \n" +
+                $"2 - Procurar usuario na lista \n" +
+                $"3 - Listar repositorios de um usuario \n" +
+                $"4 - Somar todos repositorios \n" +
+                $"5 - Listar todos os usuarios \n" +
+                $"6 - Sair \n");
+            #endregion
+
+            // User choice
+            Console.Write("Escolha: ");
+            int choice = int.Parse(Console.ReadLine()!);
         }
     }
 }
